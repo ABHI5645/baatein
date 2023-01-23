3d24b1bdeb082b0fceb14b745481621534e5af1b
abhi <abhi24.8896@gmail.com>
2023-01-23 18:32:47 +0530

    recent_chat

--- src/components/Chat/RecentChat/index.js
+++ src/components/Chat/RecentChat/index.js
@@ -1,54 +1,3 @@
-// import { Grid } from '@mui/material';
-// import React, { useContext } from 'react';
-// import { darkContext } from '../../../context/DarkmodeContext';
-// import SwipeableDrawer from '../../common/SwipableDrawer';
-// import './recentChat.css';
-// function RecentChat() {
-//   const [state, dispatch] = useContext(darkContext)
-//   const toggleTheme = (color) => {
-//     dispatch({
-//       type: color
-//     })
-//   }
-//   return (
-//     <div>
-//     <Grid 
-//     className='recentchat-container'
-//     container
-//     spacing={3}
-//     >
-//       <Grid item xs={12}>
-//         Pinned Chats
-//       </Grid>
-    
-//     {[1,2,3,4,5].map((item)=>{
-//     return(
-//     <Grid item xs={6}>
-//       <div className='recentchat-item'>
-//         <div className='recentchat-item_img new-message'>
-//           <div>
-//           <img
-//           alt="Reny Sharp"
-//           src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"/>
-//           </div>
-//         <div>
-//           name
-//         </div>
-//         </div>
-//         <div className='recentchat-message'>
-//           hey
-//         </div>
-//       </div>
-//     </Grid>
-//     )
-//     })}
-//     </Grid>
-//     <SwipeableDrawer/>
-//   </div>
-//   )
-// }
-
-// export default RecentChat
 
 import { Grid } from "@mui/material";
 import React, { useContext } from "react";
@@ -77,7 +26,7 @@ function RecentChat() {
                 <div className="recentchat-item__img">
                   <img
                     alt="Remy Sharp"
-                    src="https://lh3.googleusercontent.com/a/AEdFTp4vt6egY9bg6OFMRxW5RGLIOcPHMQ-xan1hGg8r-q8=s96-c"
+                    src="https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg"
                   />
 
                   <div>name dc sc s acascsa c</div>
